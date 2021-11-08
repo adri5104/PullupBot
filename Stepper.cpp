85cbc97c3e2b490bbb088cda9478a7a95fc5934a
Victoria Goncalves Da Corte <vicky10gd@hotmail.com>
2021-11-08 18:12:37 +0100

    stepper con millis en lugar de delay

--- Stepper.cpp
+++ Stepper.cpp
@@ -2,17 +2,8 @@
 #include <arduino.h>
 
 stepper::stepper() {
-    feed_rate = 0;
+
     is_moving = false;
-}
-
-void stepper::setFeedrate(float nfr) {
-
-    nfr = nfr * STEPS_PER_MM / 60;
-    if (feed_rate == nfr || nfr > MAX_FEEDRATE || nfr < MIN_FEEDRATE) return;
-
-    step_delay = MAX_FEEDRATE / nfr;
-    feed_rate = nfr;
 
 }
 
@@ -23,62 +14,42 @@ void stepper::setPosition(float npx) {
 }
 
 void stepper::prepareMove(float newx) {
-    maxsteps = 0;
+
     obj = (newx - px) * STEPS_PER_MM;
     absobj = abs(obj);
     if (absobj) {
         is_moving = true;
         maxsteps = absobj;
+        digitalWrite(dir_pin, obj > 0 ? HIGH : LOW);
+        step_count = 0;
+        prev_time = millis();
+        setPosition(newx);
     }
 
-    digitalWrite(dir_pin, obj > 0 ? HIGH : LOW);
-
-    delay_t = MAX_FEEDRATE / 5000;
-    accel = 1;
-    steps_to_accel = delay_t - step_delay;
-
-    if (steps_to_accel > maxsteps / 2) steps_to_accel = maxsteps / 2;
-    steps_to_decel = maxsteps - steps_to_accel;
-
-    step_count = 0;
-    prev_time = 0;
-
     if (is_moving) enableMotor();
-    setPosition(newx);
+
 }
 
 
 void stepper::move() {
 
-    unsigned long curtime = micros();
-    unsigned int _delay;
+    unsigned long curtime = millis();
 
-    if (is_moving && (curtime - prev_time) >= delay_t) {
+    if (is_moving && (curtime - prev_time) >= millisbetweensteps) {
         prev_time = curtime;
-        over = over + absobj;
-        if (over >= maxsteps) {
-            over = over - maxsteps;
-            _delay = 100;
+        if (step_count<maxsteps) {
             digitalWrite(step_pin, HIGH);
-            delayMicroseconds(_delay);
             digitalWrite(step_pin, LOW);
-            delayMicroseconds(_delay);
+            step_count++;
         }
     }
-
-    if (step_count < steps_to_accel) {
-        delay_t = delay_t - accel;
-    }
-    if (step_count >= steps_to_decel) {
-        delay_t = delay_t + accel;
-    }
-    step_count++;
 
     is_moving = !(step_count >= maxsteps);
 
     if (!is_moving) {
         disableMotor();
     }
+
 }
 
 

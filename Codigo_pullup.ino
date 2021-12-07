4f99481684b5b5f6e38ea26ba5c25146d56fe56e
Antonio Cameo <antcameo2020@gmail.com>
2021-12-07 20:17:17 +0100

    a

--- Codigo_pullup.ino
+++ Codigo_pullup.ino
@@ -79,39 +79,59 @@ void loop()
 {
   //myPullup.RobotLogic();
   //Lectura del serial
-  while(Serial.available())
-  {
-    op = Serial.parseInt();
-    a = Serial.parseFloat();
-    b = Serial.parseFloat();
-    c = Serial.parseFloat();
-    d = Serial.parseInt();
-    if (Serial.read() == '\n') break;
-  }
-  //Cosas que hace el robot.
-  //delay(20);
-  switch(op)
-  {
-    case 0:
-      myPullup.setPosicionArticulares_tics( a, b, c,d);
-    break;
-    case 1: 
-      myPullup.goHome();
-    break;
-    case 2:
-      myPullup.SerialPrintPosicionTics();
-    break;
-    case 3:
-      myPullup.setFree();
-    break;
-    case 4:
-      myPullup.SerialPrintErrores();
-    break;
-    default:
-    break;
-  }
-  op = 7;
+ if(Serial.available() > 0){ //si el serial está vacio no hace nada. Si tiene algo hace:
+    char op;    //almacena el número que puede ser 0 al 3 (en caracter)
+    op = Serial.read();
+    switch(op)
+      {        
+        
+      case '0': //este es en caso de SetFree por lo que las instrucciones son:
+        myPullup.setFree(); 
+      break;
+        
+      case '1': //Este es el caso de Homing por lo que las instrucciones son:
+        //Ejecutar la función de Homing que en nuestro caso será encender los leds 8 veces con intervalos de 0.5 segundos y al acabar poner Hecho
+        myPullup.goHome();        
+      break;
+        
+      case '2': //Este es el caso de Enviar posición por lo que las instrucciones son:
+      //Recibir 4 valores que son los pulsos de los 4 motores (Vamos a suponer que los valores los vamos a dar en float.
+      //Como lo que enviaremos desde MatLab será algo así como "2" y luego "1.11111 2.22222 3.333333 4.444444\n". Tendremos que eliminar el \n del final
 
+      float valores[4];
+          
+      for(int i=0; i<4; i++){     //cogemos los 4 valores y los almacenamos en ese valor
+        valores[i] = Serial.parseFloat(SKIP_WHITESPACE);  //Con ese comando eliminamos el espacio entre los float    (Tiene TimeOut naiss)
+      }
+
+      myPullup.setPosicionArticulares_tics( valores[0], valores[1], valores[2], valores[3]);
+
+      //Aqui esperamos a que los motores se muevan a la posición asignada para que no haya desborde de datos
+      //Y enviamos a MatLab los valores nuevos para que se actualicen los pulsos en la interfaz (que dará paso a que matlab envíe nuevos datos).
+
+      myPullup.SerialPrintPosicionTics();
+              
+      // El problema es que en el serial ahora hay dentro un terminator que hay que quitar. Ahora lo va eliminando el Serial.read de op pero no es tan eficiente.
+        
+        break;
+
+
+        case '3': //Este es el caso de Recibir posición por lo que las instrucciones son:
+
+        //Iría lo mismo que la ultima parte de la operación 2 (o no haría falta, puede usarse esta operacion) pero daré datos escritos por el serial para probar si funciona esta parte.
+        
+        myPullup.SerialPrintPosicionTics();
+
+        break;
+
+        case '4': //Este es el caso de Pedir error por lo que las intrucciones son:
+          //Aqui le eviaríamos a matlab el error
+          myPullup.SerialPrintErrores();
+         break;    
+    }
+  }
+
+ 
   #ifdef DEBUGGING_
 // myPullup.printMovidas();
  //myPullup.printGrados();
